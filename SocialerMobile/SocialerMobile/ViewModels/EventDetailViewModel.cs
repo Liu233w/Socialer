4b7271ce92d73f6bc5d8f70aed2244e00d7af391
Liu233w <gh@liu233w.com>
2021-04-10 20:52:52 +1000

    event view models

---
+++ SocialerMobile/SocialerMobile/ViewModels/EventDetailViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SocialerMobile.ViewModels
+{
+    public class EventDetailViewModel : BaseEventViewModel
+    {
+        private string _eventId;
+        public string Id { get; set; }
+
+        public string EventId
+        {
+            get { return _eventId; }
+            set
+            {
+                _eventId = value;
+                LoadEventId(value);
+            }
+        }
+
+        public async void LoadEventId(string itemId)
+        {
+            try
+            {
+                var item = await SocialerDataStore.GetEventAsync(itemId);
+                Id = item.Id;
+                Name = item.Name;
+                Note = item.Note;
+                RatingChange = item.RatingChange;
+            }
+            catch (Exception)
+            {
+                Debug.WriteLine("Failed to Load Item");
+            }
+        }
+    }
+}
---
+++ SocialerMobile/SocialerMobile/ViewModels/NewEventViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using SocialerMobile.Models;
+using Xamarin.Forms;
+
+namespace SocialerMobile.ViewModels
+{
+    public class NewEventViewModel : BaseEventViewModel
+    {
+        public string PersonId { get; set; }
+
+        public NewEventViewModel()
+        {
+            SaveCommand = new Command(OnSave, ValidateSave);
+            CancelCommand = new Command(OnCancel);
+            this.PropertyChanged +=
+                (_, __) => SaveCommand.ChangeCanExecute();
+
+            RatingChange = 0;
+            Time = DateTime.Now;
+        }
+
+
+        private bool ValidateSave()
+        {
+            return !string.IsNullOrWhiteSpace(Name);
+        }
+
+        private async void OnCancel()
+        {
+            // This will pop the current page off the navigation stack
+            await Shell.Current.GoToAsync("..");
+        }
+
+        private async void OnSave()
+        {
+            await SocialerDataStore.AddEventAsync(PersonId, new Event
+            {
+                Name = Name,
+                Note = Note,
+                Time = Time,
+                RatingChange = RatingChange,
+            });
+
+            await Shell.Current.GoToAsync("..");
+        }
+
+        public Command SaveCommand { get; }
+        public Command CancelCommand { get; }
+    }
+}
